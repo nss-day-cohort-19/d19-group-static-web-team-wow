876dad6476cc2deb66c298626666a846bd7c5b6d
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-04-13 13:26:42 -0500

    I'm in the middle of rewriting/structuring my functions. i didn't mean to stage those changes! Ignore the buttons.js for now. I just haven't commited in a while.

--- js/buttons.js
+++ js/buttons.js
@@ -1,6 +1,15 @@
 "use strict";
 
 var btmBtn = document.getElementById('footerBtn');
+
 btmBtn.addEventListener('click', function() {
   window.scrollTo(0, 0);
+});
+
+var text = document.getElementById('userInput')
+var userInput = document.getElementById('contactForm', userInput.addEventListener('{input}'));
+var contactBtn = document.getElementById('submit-form');
+
+contactBtn.addEventListener('click', function(userInput) {
+  console.log('{input}');
 });
