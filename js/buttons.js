9b95071a2aef44ce29c78c870555bcc2d3bc99c6
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-04-13 14:37:53 -0500

    this

--- js/buttons.js
+++ js/buttons.js
@@ -6,10 +6,16 @@ btmBtn.addEventListener('click', function() {
   window.scrollTo(0, 0);
 });
 
-var text = document.getElementById('userInput')
-var userInput = document.getElementById('contactForm', userInput.addEventListener('{input}'));
+
+
+
+//*doesnt work yet*//
+
 var contactBtn = document.getElementById('submit-form');
 
-contactBtn.addEventListener('click', function(userInput) {
-  console.log('{input}');
-});
+var userSubmit = document.querySelector('form.contactForm')
+  contactBtn.addEventListener('click', function() {
+      console.log(document.getElementsByClass('userInput').getAttribute('value'));
+    });
+  
+
